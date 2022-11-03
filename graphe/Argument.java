8c5f48db35649884ff879f44b0d25ad88fe3a652
Ethan0991 <98706163+Ethan0991@users.noreply.github.com>
2022-11-03 14:19:16 +0100

    Add files via upload

---
+++ graphe/Argument.java
@@ -0,0 +1,39 @@
+package ProjetDebat.graphe;
+
+
+public class Argument implements Comparable<Argument> {
+	
+	private static int numArg=0 ;
+	
+	private String arg;
+	private int num=0;
+	//private List<Argument> argContradiction ;
+	
+	public Argument(String arg) {
+		
+		//argContradiction =  new ArrayList<Argument>();
+		this.arg=arg;
+		num = numArg;
+		numArg++;
+	}
+
+	public String toString() {
+		return "Argument [arg=" + arg + ", num=" + (num+1) + "]";
+	}
+
+	@Override
+	public int compareTo(Argument A) {
+		
+		return this.num - A.num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+	
+	
+	
+	
+
+	
+}
